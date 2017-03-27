9dc3d0810fb491fb5915e9335d5892b834f959ea
Nicolle Cysneiros <nicolle.cysneiros@gmail.com>
2017-03-27 14:52:21 -0300

    Configures test for Search component

--- github-app/src/components/Search.vue
+++ github-app/src/components/Search.vue
@@ -5,9 +5,7 @@
         <div class="logo-container">
           <img class="logo" src="./../assets/github-mark.png" alt="github logo">  
         </div>
-        <h1 class="title">
-          Github APP
-        </h1>
+        <h1 class="title">Github APP</h1>
         <div class="content">
           <div class="field has-addons">
             <input class="input" type="text" placeholder="Search users or repos" v-model="searchToken">
